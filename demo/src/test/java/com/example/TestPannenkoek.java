dfc36790442d107e7a995f00fc78126adb5010b8
BarmanTurbo <21035407@student.hhs.nl>
2023-02-20 15:08:39 +0100

    test pannenkoeken

--- demo/src/test/java/com/example/TestPannenkoek.java
+++ demo/src/test/java/com/example/TestPannenkoek.java
@@ -2,6 +2,7 @@ package com.example;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
 
@@ -28,6 +29,33 @@ public class TestPannenkoek
         teststapel.nieuwPannenkoekOpBord(teststring);
 
         assertEquals(teststapel.pakPannenkoekVanBord(),teststring);
+    }
+
+    public void test2itemsopdestackNietEquals(){
+        Stack teststapel = new Stack();
+
+        String teststring1 = "testwaarde1";
+        String teststring2 = "testwaarde2";
+
+        teststapel.nieuwPannenkoekOpBord(teststring1);
+        teststapel.nieuwPannenkoekOpBord(teststring2);
+
+        assertNotEquals(teststring1, teststapel.pakPannenkoekVanBord());
+    }
+
+    public void test2itemsopdestackWelEquals(){
+        Stack teststapel = new Stack();
+
+        String teststring1 = "t1";
+        String teststring2 = "t2";
+
+        teststapel.nieuwPannenkoekOpBord(teststring1);
+        teststapel.nieuwPannenkoekOpBord(teststring2);
+
+        Object o1 = teststapel.pakPannenkoekVanBord();
+        Object o2 = teststapel.pakPannenkoekVanBord();
+
+        assertEquals(teststring1, o2);
     }
 
     
