0e70ea23238d8ea79312b8fb43b4404c58c5228d
BarmanTurbo <21035407@student.hhs.nl>
2023-02-22 13:42:18 +0100

    check if my pawn

--- demo/src/main/java/com/example/Speler.java
+++ demo/src/main/java/com/example/Speler.java
@@ -6,7 +6,7 @@ public class Speler {
     private String kleur;
     private String naam;
 
-    public Speler(String kleur, String naam){
+    public Speler(String kleur, String naam) {
         this.kleur = kleur;
         this.naam = naam;
     }
@@ -14,67 +14,70 @@ public class Speler {
     public String getKleur() {
         return kleur;
     }
+
     public String getNaam() {
         return naam;
     }
-    
+
     public int countOwn() {
         int count = 0;
         for (int i = 0; i < Speelveld.kolommen; i++) {
             for (int j = 0; j < Speelveld.rijen; j++) {
                 Pion pion = Speelveld.veldPionnen[i][j];
-                if (pion != null && pion.getKleur().equals(this.kleur)){
+                if (pion != null && pion.getKleur().equals(this.kleur)) {
                     count++;
                 }
             }
         }
         return count;
     }
-    
-    public boolean geenLegalMoveBeschikbaar(){
+
+    public boolean geenLegalMoveBeschikbaar() {
         ArrayList<Pion> alJouwPionnen = new ArrayList<Pion>();
-        for(int i=0;i<Speelveld.rijen;i++){
-            for(int j=0;j<Speelveld.kolommen;j++){
-                if(Speelveld.veldPionnen[i][j]!=null){
-                    alJouwPionnen.add(Speelveld.veldPionnen[i][j]);
+        for (int i = 0; i < Speelveld.rijen; i++) {
+            for (int j = 0; j < Speelveld.kolommen; j++) {
+                if (Speelveld.veldPionnen[i][j] != null) {
+                    if (Speelveld.veldPionnen[i][j].getKleur() == getKleur()) {
+                        alJouwPionnen.add(Speelveld.veldPionnen[i][j]);
+                    }
                 }
             }
         }
-        //Nu heb je al jouw pionnen in een lijstje staan.
-        for(Pion p : alJouwPionnen){
-            if( 
-                //Controleer voor alle sprongen
-                //Check plek 2 boven deze plek
-                (Speelveld.legalDupliceer(p.getXPos(),p.getYPos(),p.getXPos()+2,p.getYPos()))
-                ||//Check plek 2 rechts van deze plek
-                (Speelveld.legalDupliceer(p.getXPos(),p.getYPos(),p.getXPos(),p.getYPos()+2))
-                ||//Check plek 2 onder deze plek
-                (Speelveld.legalDupliceer(p.getXPos(),p.getYPos(),p.getXPos()-2,p.getYPos()))
-                ||//Check plek 2 links van deze plek
-                (Speelveld.legalDupliceer(p.getXPos(),p.getYPos(),p.getXPos(),p.getYPos()-2))
-                ){
+        // Nu heb je al jouw pionnen in een lijstje staan.
+        for (Pion p : alJouwPionnen) {
+            if (
+            // Controleer voor alle sprongen
+            // Check plek 2 boven deze plek
+            (Speelveld.legalDupliceer(p.getXPos(), p.getYPos(), p.getXPos() + 2, p.getYPos()))
+                    || // Check plek 2 rechts van deze plek
+                    (Speelveld.legalDupliceer(p.getXPos(), p.getYPos(), p.getXPos(), p.getYPos() + 2))
+                    || // Check plek 2 onder deze plek
+                    (Speelveld.legalDupliceer(p.getXPos(), p.getYPos(), p.getXPos() - 2, p.getYPos()))
+                    || // Check plek 2 links van deze plek
+                    (Speelveld.legalDupliceer(p.getXPos(), p.getYPos(), p.getXPos(), p.getYPos() - 2))) {
                 return false;
             }
-            if(
-                //Controleer voor alle aanloggende vakjes of dupliceren kan
-                (Speelveld.legalDupliceer(p.getXPos(),p.getYPos(),p.getXPos()-1,p.getYPos()-1))
-                ||
-                (Speelveld.legalDupliceer(p.getXPos(),p.getYPos(),p.getXPos()-1,p.getYPos()))
-                ||
-                (Speelveld.legalDupliceer(p.getXPos(),p.getYPos(),p.getXPos()-1,p.getYPos()+1))
-                ||
-                (Speelveld.legalDupliceer(p.getXPos(),p.getYPos(),p.getXPos(),p.getYPos()-1))
-                ||
-                (Speelveld.legalDupliceer(p.getXPos(),p.getYPos(),p.getXPos(),p.getYPos()))
-                ||
-                (Speelveld.legalDupliceer(p.getXPos(),p.getYPos(),p.getXPos(),p.getYPos()+1))
-                ||
-                (Speelveld.legalDupliceer(p.getXPos(),p.getYPos(),p.getXPos()+1,p.getYPos()-1))
-                ||
-                (Speelveld.legalDupliceer(p.getXPos(),p.getYPos(),p.getXPos()+1,p.getYPos()))
-                ||
-                (Speelveld.legalDupliceer(p.getXPos(),p.getYPos(),p.getXPos()+1,p.getYPos()+1))
-            ){return false;}
+            if (
+            // Controleer voor alle aanloggende vakjes of dupliceren kan
+            (Speelveld.legalDupliceer(p.getXPos(), p.getYPos(), p.getXPos() - 1, p.getYPos() - 1))
+                    ||
+                    (Speelveld.legalDupliceer(p.getXPos(), p.getYPos(), p.getXPos() - 1, p.getYPos()))
+                    ||
+                    (Speelveld.legalDupliceer(p.getXPos(), p.getYPos(), p.getXPos() - 1, p.getYPos() + 1))
+                    ||
+                    (Speelveld.legalDupliceer(p.getXPos(), p.getYPos(), p.getXPos(), p.getYPos() - 1))
+                    ||
+                    (Speelveld.legalDupliceer(p.getXPos(), p.getYPos(), p.getXPos(), p.getYPos()))
+                    ||
+                    (Speelveld.legalDupliceer(p.getXPos(), p.getYPos(), p.getXPos(), p.getYPos() + 1))
+                    ||
+                    (Speelveld.legalDupliceer(p.getXPos(), p.getYPos(), p.getXPos() + 1, p.getYPos() - 1))
+                    ||
+                    (Speelveld.legalDupliceer(p.getXPos(), p.getYPos(), p.getXPos() + 1, p.getYPos()))
+                    ||
+                    (Speelveld.legalDupliceer(p.getXPos(), p.getYPos(), p.getXPos() + 1, p.getYPos() + 1))) {
+                return false;
+            }
         }
         return true;
     }
